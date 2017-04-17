177ddf4523afa873c8d4d9585e34cc730e0918ab
natasha <paranyak@ucu.edu.ua>
2017-04-17 18:01:21 +0300

    added HowToUse instruction file

--- example.cpp
+++ example.cpp
@@ -14,43 +14,45 @@
 using namespace std;
 
 mutex mx;
-float allS = 0;
+double allS = 0;
 int m = 5;
 
 
-float summing(float x1, float x2)
+double summing(double x1, double x2)
 {
-    float firstS = 0;
-    float secondS= 0;
-    float generalS;
+    double firstS = 0;
+    double secondS= 0;
+    double generalS = 0;
     for (int i = 1; i <= m; ++i)
     {
+
         firstS += i * cos((i + 1) * x1 + 1);
         secondS += i * cos((i + 1) * x2 + 1);
     }
-
-    generalS = -(firstS * secondS);
-
-    //cout <<"x1  " <<x1<< ", x2  " << x2 << " , sum : " <<generalS<< endl;
+    generalS += -(firstS) * (secondS);
+    //cout <<"x1  " <<x1<< ", x2  " << x2 << " , sum : " <<generalS<< "\n" <<endl;
     return generalS;
 }
 
-void *integration (float xStart, float xEnd, float yStart, float yEnd, float stepX , float stepY){
+void *integration (double xStart, double xEnd, double yStart, double yEnd, double stepX , double stepY){
 
     //початок і кінець інтегрування, крок між х і-тим та х і-1
     //дельта це stepX/Y
     //з шапочкою це 2 стартових + крок
 
-    mx.lock();
-    float sum = 0;
+
+
+    double sum = 0;
     while(xStart <= xEnd){
-        float yS = yStart;
+        double yS = yStart;
         while(yS <= yEnd){
-            sum += summing((2 * xStart + stepX)/2, (2*yS + stepY)/2)*stepX*stepY;
+            sum += summing((2 * xStart + stepX)/2, (2*yS + stepY)/2 )*stepX*stepY;
+            //cout << xStart << "<-- XXXXXXX  " << yS << "<-- YYYYYYYY"<< endl;
             yS += stepY;
         }
         xStart += stepX;
     }
+    mx.lock();
     allS += sum;
     mx.unlock();
 }
@@ -64,58 +66,93 @@ int main(){
     string word;
 
 
-    myfile.open("/home/natasha/CLionProjects/threads_project_2/read.txt");    //файл з усма конфігураціями
+    myfile.open("read.txt");    //файл з усма конфігураціями
     if (!myfile.is_open()) {
         cerr << "Error" << endl;
     }
     while (myfile >> word) {        //додаємо рядок без =
-        size_t pos = word.find("=");
-        string str3 = word.substr(pos+1);
-        words.push_back(str3);
+        words.push_back(word);
     }
 
-    int N = atoi( words[2].c_str() );//кількість потоків
+    long xS, yS, xE, yE;
+    int N = 2;
+    double abs, rel;
+    for (int i = 0; i < words.size(); ++i) {
+        //cout << words[i] << ' ';
+        if (words[i].find("xStart=") != std::string::npos) {
+            size_t pos = words[i].find("xStart=");
+             xS  = atoi(words[i].substr(pos + 7).c_str());
+        }
+        if (words[i].find("yStart=") != std::string::npos) {
+            size_t pos = words[i].find("yStart=");
+            yS  = atoi(words[i].substr(pos + 7).c_str());
+        }
+        if (words[i].find("xEnd=") != std::string::npos) {
+            size_t pos = words[i].find("xEnd=");
+            xE = atoi(words[i].substr(pos + 5).c_str());
+        }
+        if (words[i].find("yEnd=") != std::string::npos) {
+            size_t pos = words[i].find("yEnd=");
+            yE = atoi(words[i].substr(pos + 5).c_str());
+        }
+       if (words[i].find("m=") != std::string::npos) {
+            size_t pos = words[i].find("m=");
+            m = atoi(words[i].substr(pos + 2).c_str());
+        }
+        if (words[i].find("threads=") != std::string::npos) {
+            size_t pos = words[i].find("threads=");
+            N = atoi(words[i].substr(pos + 8).c_str());
+        }
+        if (words[i].find("absolute=") != std::string::npos) {
+            size_t pos = words[i].find("absolute=");
+            abs = atoi(words[i].substr(pos + 9).c_str());
+        }
+        if (words[i].find("relative=") != std::string::npos) {
+            size_t pos = words[i].find("relative=");
+            rel = atoi(words[i].substr(pos + 9).c_str());
+        }
+
+
+
+    }
 
     thread my_thread[N];
     long id;
-    long xS = atoi( words[3].c_str() );        //зчитувати з файлу   початки-кінці інтегрування
-    long yS = atoi( words[5].c_str() );
-    long xE = atoi( words[4].c_str() );
-    long yE = atoi( words[6].c_str() );
-    m = atoi( words[7].c_str() );
-    float stepX = (xE - xS)/N;      //приблизна межа для кожного інтегралу
-    float stepY = (yE - yS)/N;
+    double stepX = (xE - xS)/N;      // межа для кожного інтегралу
+    double stepY = (yE - yS)/N;
 
     //потоки
     for(id = 0; id < N; id++){
-        float xEnd = xS + stepX;        //кінець для локальної межі
-        float stepingX = stepX/1000;    //крок між і-тим та і-1-шим
-        float yEnd = yS + stepY;
-        float stepingY = stepY/1000;
+        double stepingX = 1/(stepX*1000);    //крок між і-тим та і-1-шим
+        double xEnd = xS +stepX*(id+1) + stepingX*id;        //кінець для локальної межі
+        //cout<< "!!!!!!! step" << stepingX;
+        double stepingY = 1/(stepY*1000);
+        double yEnd = yS +stepY*(id+1) + stepingY*id;
         if (xEnd > xE){
             xEnd = xE;
         }if(yEnd > yE){
             yEnd = yE;
         }
-        my_thread[id] =  thread(integration, xS, xEnd, yS, yEnd, stepingX, stepingY);
-        xS += stepX + stepingX;     //наступний початок - це крок між межами для потоків + крок для локальних меж
-        yS += stepY + stepingY;
+        my_thread[id] =  thread(integration, xS + stepX*id + stepingX*id, xEnd, yS +stepY*(id) + stepingY*id, yEnd, stepingX, stepingY);
+
     }
-    for(id = 0; id < N; id++){
-        my_thread[id].join();
-    }
+    for (auto& th : my_thread) th.join();
     auto finish_time = get_current_time_fenced();
     auto all_time = finish_time - stage1_start_time;
     cout << "ALL time: " << to_us(all_time) << endl;
 
     fstream log;
-    log.open("/home/natasha/CLionProjects/threads_project_2/result.txt", fstream::app);
+    log.open("result.txt", fstream::app);
     std::chrono::duration<double, std::milli> all_ms = all_time;
     log << "Result: " << allS<< " ";
     log << all_ms.count();
     log <<"\n";
     log.close();
-    cout<< "Result : "<<allS;
+    cout<< "Result : "<<allS << "\n";
+    cout << "Absolute and relative values (observational error): " << abs << " " << rel << "\n";
+    if((abs/(allS + abs)) <= rel || (abs/(allS - abs)) <= rel){
+        cout << "All done right) integral is without observational error";
+    }
 }
 
 
--- toRun.py
+++ toRun.py
@@ -1,10 +1,39 @@
 import subprocess
 import math
+import os
+
 subprocess.call(["g++", "/home/natasha/CLionProjects/threads_project_2/example.cpp","-o", "example" ,"-std=c++11", "-pthread"])
 print("Compiled")
+os.chdir("./cmake-build-debug")
+configType = str(input("Do you want to open config file(f) or enter from console(c)? f/c "))
+if configType == "c":
+	f = open("./read.txt", "w")
+	abs=str(input("Enter absolute value, please... "))
+	rel=str(input("Enter relative value, please... "))
+	xS=str(input("Enter x starting point , please... "))
+	xE=str(input("Enter x ending point, please... "))
+	yS=str(input("Enter y  starting point , please... "))
+	yE=str(input("Enter y ending point, please... "))
+	m=str(input("Enter m , please... "))
+	N=str(input("Enter number of threads, please... "))
+
+	f.write('absolute='+abs+'\n')
+	f.write('relative='+rel +'\n')
+	f.write('xStart='+xS+'\n')
+	f.write('threads='+N + '\n')
+	f.write('yStart='+yS+'\n')
+	f.write('xEnd='+xE +'\n')
+	f.write('yEnd='+yE+'\n')
+	f.write('m='+m + '\n')
+	f.close()
+else:
+	f = open("./read.txt", "r")
+	print("It's yours configuration...")
+	for line in f:
+		print(line)
 number = int(input("How many times do you want to run file: "))
 while number > 0: 
-	subprocess.call("./example")
+	subprocess.call("../example")
 	number -= 1
 
 f= open("./result.txt", "r")
@@ -12,10 +41,11 @@ words = []
 for line in f:
         words.append([str(n) for n in line.strip().split(' ')])
 all_min = math.inf
+
 for n in range(len(words)):
 	if float(words[n][2]) < all_min:
 		all_min = float(words[n][2])
 
-print("----- Minimum time at all -----")
+print("\n" + "----- Minimum time at all -----")
 print("All time: " + str(all_min))
 
